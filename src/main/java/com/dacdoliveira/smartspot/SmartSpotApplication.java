cf5b70138a23b7461fe3388e32196e1bfca8294f
Diers <Diers@LAPTOP-VTCD2RNQ>
2021-07-31 20:21:02 -0300

    cria estrutura inicial do projeto padrao springboot

---
+++ src/main/java/com/dacdoliveira/smartspot/SmartSpotApplication.java
@@ -0,0 +1,15 @@
+package com.dacdoliveira.smartspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartSpotApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SmartSpotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dacdoliveira/smartspot/SmartSpotApplicationTests.java
@@ -0,0 +1,15 @@
+package com.dacdoliveira.smartspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartSpotApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
